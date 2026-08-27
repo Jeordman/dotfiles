aa65713e90ea93a3367b86b93b91cdefeca3d1f6
Jeordin Callister <callisterjeordin@gmail.com>
2026-08-27 17:36:29 -0400

    colors

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -16,6 +16,8 @@ return {
   },
   config = function()
     require('neo-tree').setup {
+      enable_git_status = true,
+      git_status_async = true,
       window = {
         position = 'right',
         width = 40,
@@ -25,6 +27,56 @@ return {
           ['/'] = 'noop',
         },
       },
+      default_component_configs = {
+        -- color the file NAME by git status, not just the trailing symbol
+        name = {
+          use_git_status_colors = true,
+        },
+        git_status = {
+          symbols = {
+            added     = '+',
+            modified  = '~',
+            deleted   = '_',
+            renamed   = '→',
+            untracked = '?',
+            ignored   = '',
+            unstaged  = '',
+            staged    = '●',
+            conflict  = '!',
+          },
+        },
+      },
     }
+
+    -- Every file name is white. Git status recolors the text only, no
+    -- backgrounds: blue = modified, green = added/untracked, red = deleted.
+    -- Reapplied on ColorScheme so a theme switch can't wipe them.
+    local white = '#c6c1a9'
+    local blue = '#7aa2f7'
+    local green = '#a6e3a1'
+    local red = '#f38ba8'
+
+    local function set_neotree_git_hl()
+      vim.api.nvim_set_hl(0, 'NeoTreeFileName', { fg = white })
+      vim.api.nvim_set_hl(0, 'NeoTreeFileNameOpened', { fg = white })
+      vim.api.nvim_set_hl(0, 'NeoTreeDirectoryName', { fg = white })
+      vim.api.nvim_set_hl(0, 'NeoTreeDirectoryIcon', { fg = white })
+      vim.api.nvim_set_hl(0, 'NeoTreeRootName', { fg = white, bold = true })
+
+      vim.api.nvim_set_hl(0, 'NeoTreeGitAdded', { fg = green })
+      vim.api.nvim_set_hl(0, 'NeoTreeGitStaged', { fg = green })
+      vim.api.nvim_set_hl(0, 'NeoTreeGitUntracked', { fg = green })
+      vim.api.nvim_set_hl(0, 'NeoTreeGitModified', { fg = blue })
+      vim.api.nvim_set_hl(0, 'NeoTreeGitUnstaged', { fg = blue })
+      vim.api.nvim_set_hl(0, 'NeoTreeGitRenamed', { fg = blue })
+      vim.api.nvim_set_hl(0, 'NeoTreeGitDeleted', { fg = red })
+      vim.api.nvim_set_hl(0, 'NeoTreeGitConflict', { fg = red, bold = true })
+    end
+
+    set_neotree_git_hl()
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      callback = set_neotree_git_hl,
+      desc = 'Reapply Neo-tree git highlights',
+    })
   end,
 }
