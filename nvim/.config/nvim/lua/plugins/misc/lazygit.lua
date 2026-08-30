9dc880794cf37fb32f677af0bab262a2d95c6779
Jeordin Callister <callisterjeordin@gmail.com>
2026-08-30 17:57:15 -0400

    make lazygit bootyful

--- nvim/.config/nvim/lua/plugins/misc/lazygit.lua
+++ nvim/.config/nvim/lua/plugins/misc/lazygit.lua
@@ -17,4 +17,18 @@ return {
   keys = {
     { '<leader>ga', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
   },
+  config = function()
+    -- lazygit has no background setting of its own, so the float's bg is what
+    -- shows through its panels. #161616 is herdr's sidebar_bg.
+    local function set_lazygit_hl()
+      vim.api.nvim_set_hl(0, 'LazyGitFloat', { bg = '#161616' })
+      vim.api.nvim_set_hl(0, 'LazyGitBorder', { fg = '#3A3A3A', bg = '#161616' })
+    end
+
+    set_lazygit_hl()
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      callback = set_lazygit_hl,
+      desc = 'Reapply LazyGit float highlights',
+    })
+  end,
 }
